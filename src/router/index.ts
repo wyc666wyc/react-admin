5a01971ec0e58c329de346b5b5e5fb76046f11ac
wuyuc <wuyuc@shencai-china.com>
2023-08-14 09:56:04 +0800

    perf router ts declare

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,19 @@
 import { useRoutes } from 'react-router-dom'
 import { RouterProps } from '@/router/interface/index'
 
-const metaRouters = import.meta.glob('./modules/*.tsx', { eager: true })
+const metaRouters = import.meta.glob('./modules/*.tsx', { eager: true }) as Record<string, Record<string, RouterProps[]>>
 
 export const routerArray: RouterProps[] = []
 Object.keys(metaRouters).forEach(item => {
   Object.keys(metaRouters[item]).forEach(key => {
-    routerArray.push(...metaRouters[item][key])
+    const routes = metaRouters[item][key]
+    routerArray.push(...routes)
   })
 })
 const rootRouter = [
   ...routerArray
 ]
+
 const Router = () => {
   const routes = useRoutes(rootRouter)
   return routes
